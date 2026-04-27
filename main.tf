18595d6cedacf1f3423f61f2002d341631a93c5a
wonderv0id <90965012+awab-hassan@users.noreply.github.com>
2026-04-27 23:18:32 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Variables for configurationsssssssssssssssss
+# Variables for configurationssssssssssssssssssssss
 variable "hongkong_vpc_cidr" {
   description = "CIDR block for the Region 2 VPC"
   type        = string
